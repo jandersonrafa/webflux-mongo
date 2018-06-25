5bef48576b4879d46e4c49c74ee335e36d91dc1a
Adrisson Silva <adrisson.silva@outlook.com>
2018-06-24 22:09:12 -0300

    Validação de evento vigente

--- src/main/java/com/article/article/service/article/ArticleServiceImpl.java
+++ src/main/java/com/article/article/service/article/ArticleServiceImpl.java
@@ -38,26 +38,26 @@ import reactor.core.publisher.Mono;
 @Service
 public class ArticleServiceImpl implements ArticleService {
 
-	@Autowired
-	private ArticleRepository articleRepository;
+    @Autowired
+    private ArticleRepository articleRepository;
 
-	@Autowired
-	private ArticleSimpleRepository articleSimpleRepository;
+    @Autowired
+    private ArticleSimpleRepository articleSimpleRepository;
 
-	@Autowired
-	private ModelMapper modelMapper;
+    @Autowired
+    private ModelMapper modelMapper;
 
-	@Autowired
-	private ValidatorService validatorService;
+    @Autowired
+    private ValidatorService validatorService;
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@Autowired
-	private EventService eventService;
+    @Autowired
+    private EventService eventService;
 
-	@Autowired
-	private FileService fileService;
+    @Autowired
+    private FileService fileService;
 
 //	@Override
 //	public Flux<SubmissionListingDto> findEventWithArticleLoggedUser() {
@@ -70,82 +70,89 @@ public class ArticleServiceImpl implements ArticleService {
 //			return submission;
 //		});
 //	}
-	@Override
-	public Flux<SubmissionListingDto> findEventWithArticleLoggedUser() {
-		final String loggedUserId = userService.getLoggedUser().getId();
+    @Override
+    public Flux<SubmissionListingDto> findEventWithArticleLoggedUser() {
+        final String loggedUserId = userService.getLoggedUser().getId();
 //		final List<Article> articles = articleRepository.findAll().collect(Collectors.toList()).block();
-		final List<Article> articles = articleRepository.findByUser_id(loggedUserId).sort().collect(Collectors.toList()).block();
-		//		final List<Article> articles = articleRepository.findByUser_id(loggedUserId);
-		final Set<Event> eventsWithSumissionLoggedUser = articles.stream().map(Article::getEvent).filter(Objects::nonNull).collect(toSet());
-		// TODO filtrar por eventos do usuario
-		return Flux.just(eventsWithSumissionLoggedUser.toArray(new Event[eventsWithSumissionLoggedUser.size()])).map(entity -> {
-			final SubmissionListingDto dto = modelMapper.map(entity, SubmissionListingDto.class);
-			return dto;
-		});
-	}
+        final List<Article> articles = articleRepository.findByUser_id(loggedUserId).sort().collect(Collectors.toList()).block();
+        //		final List<Article> articles = articleRepository.findByUser_id(loggedUserId);
+        final Set<Event> eventsWithSumissionLoggedUser = articles.stream().map(Article::getEvent).filter(Objects::nonNull).collect(toSet());
+        // TODO filtrar por eventos do usuario
+        return Flux.just(eventsWithSumissionLoggedUser.toArray(new Event[eventsWithSumissionLoggedUser.size()])).map(entity -> {
+            final SubmissionListingDto dto = modelMapper.map(entity, SubmissionListingDto.class);
+            return dto;
+        });
+    }
 
-	@Override
-	public Mono<SubmissionDetailDto> findById(String articleId) {
-		return articleRepository.findById(articleId).map(entity -> {
-			final SubmissionDetailDto dto = modelMapper.map(entity, SubmissionDetailDto.class);
-			dto.setEvent(entity.getEvent());
-			return dto;
-		});
-	}
+    @Override
+    public Mono<SubmissionDetailDto> findById(String articleId) {
+        return articleRepository.findById(articleId).map(entity -> {
+            final SubmissionDetailDto dto = modelMapper.map(entity, SubmissionDetailDto.class);
+            dto.setEvent(entity.getEvent());
+            return dto;
+        });
+    }
 
-	@Override
-	public List<Article> findByEventId(String eventId) {
-		return articleSimpleRepository.findByEvent_EventId(eventId);
-	}
+    @Override
+    public List<Article> findByEventId(String eventId) {
+        return articleSimpleRepository.findByEvent_EventId(eventId);
+    }
 
-	@Override
-	public void delete(String submissionId) {
-		final Optional<Article> optional = articleRepository.findById(submissionId).blockOptional();
-		final String loggedUserId = userService.getLoggedUser().getId();
-		if (optional.isPresent() && optional.get().getUser().getId().equals(loggedUserId)) {
-			articleRepository.delete(optional.get()).block();
-		} else {
-			throw new NotFoundException("Artigo não encontrado ou não permitido");
-		}
-	}
+    @Override
+    public void delete(String submissionId) {
+        final Optional<Article> optional = articleRepository.findById(submissionId).blockOptional();
+        final String loggedUserId = userService.getLoggedUser().getId();
+        if (optional.isPresent() && optional.get().getUser().getId().equals(loggedUserId)) {
+            articleRepository.delete(optional.get()).block();
+        } else {
+            throw new NotFoundException("Artigo não encontrado ou não permitido");
+        }
+    }
 
-	@Override
-	public Mono<SubmissionDetailDto> update(String articleId, SubmissionDetailDto dto) {
-		validatorService.validate(dto);
-		final Optional<Article> optional = articleRepository.findById(articleId).blockOptional();
-		final String loggedUserId = userService.getLoggedUser().getId();
-		if (optional.isPresent() && optional.get().getUser().getId().equals(loggedUserId)) {
-			final Article model = optional.get();
-			if (dto.getFile() != null && !Strings.isNullOrEmpty(dto.getFile().getOriginalFilename())) {
-				model.setFileName(fileService.save(dto.getFile()));
-			}
+    @Override
+    public Mono<SubmissionDetailDto> update(String articleId, SubmissionDetailDto dto) {
+        validatorService.validate(dto);
+        final Optional<Article> optional = articleRepository.findById(articleId).blockOptional();
+        final String loggedUserId = userService.getLoggedUser().getId();
+        if (optional.isPresent() && optional.get().getUser().getId().equals(loggedUserId)) {
+            final Article model = optional.get();
+            if (dto.getFile() != null && !Strings.isNullOrEmpty(dto.getFile().getOriginalFilename())) {
+                model.setFileName(fileService.save(dto.getFile()));
+            }
 
-			model.setResume(dto.getResume());
-			model.setSubmissionDate(LocalDate.now());
-			model.setTitle(dto.getTitle());
-			model.setUser(userService.getLoggedUser());
-			return articleRepository.save(model).map(entity -> modelMapper.map(entity, SubmissionDetailDto.class));
-		} else {
-			throw new NotFoundException("Artigo não encontrado ou não permitido");
-		}
-	}
+            model.setResume(dto.getResume());
+            model.setSubmissionDate(LocalDate.now());
+            model.setTitle(dto.getTitle());
+            model.setUser(userService.getLoggedUser());
+            return articleRepository.save(model).map(entity -> modelMapper.map(entity, SubmissionDetailDto.class));
+        } else {
+            throw new NotFoundException("Artigo não encontrado ou não permitido");
+        }
+    }
 
-	@Override
-	public Mono<SubmissionDetailDto> create(SubmissionDetailDto dto, String idEvento) {
-		validatorService.validate(dto);
+    @Override
+    public Mono<SubmissionDetailDto> create(SubmissionDetailDto dto, String idEvento) {
+        validatorService.validate(dto);
 
-		if (dto.getFile() == null || Strings.isNullOrEmpty(dto.getFile().getOriginalFilename())) {
-			throw new BadRequestException("Arquivo é obrigatório");
-		}
-		final String filename = fileService.save(dto.getFile());
-		dto.setFileName(filename);
-		modelMapper.getConfiguration()
-				.setMatchingStrategy(MatchingStrategies.STRICT);
-		final Article model = modelMapper.map(dto, Article.class);
+        if (dto.getFile() == null || Strings.isNullOrEmpty(dto.getFile().getOriginalFilename())) {
+            throw new BadRequestException("Arquivo é obrigatório");
+        }
 
-		model.setUser(userService.getLoggedUser());
-		model.setEvent(eventService.findModelById(idEvento));
-		model.setSubmissionDate(LocalDate.now());
-		return articleRepository.save(model).map(entity -> modelMapper.map(entity, SubmissionDetailDto.class));
-	}
+        Event findModelById = eventService.findModelById(idEvento);
+        if (findModelById.getRegistrationStartDate().isAfter(LocalDate.now())
+                || findModelById.getRegistrationEndDate().isBefore(LocalDate.now())) {
+            throw new BadRequestException("Evento fora da vigência!");
+        }
+
+        final String filename = fileService.save(dto.getFile());
+        dto.setFileName(filename);
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        final Article model = modelMapper.map(dto, Article.class);
+
+        model.setUser(userService.getLoggedUser());
+        model.setEvent(findModelById);
+        model.setSubmissionDate(LocalDate.now());
+        return articleRepository.save(model).map(entity -> modelMapper.map(entity, SubmissionDetailDto.class));
+    }
 }
